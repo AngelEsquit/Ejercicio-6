d1038f713a6e8df913ae11b2dbd0614f257c51aa
AngelEsquit <esq23221@uvg.edu.gt>
2023-11-01 19:40:38 -0600

    Update Telefono.java

--- Telefono.java
+++ Telefono.java
@@ -1,3 +1,35 @@
-public class Telefono {
+public class Telefono implements DispositivoElectronico{
+    private String modelo;
+    private boolean estado = true;
+
+    public Telefono(String modelo, boolean estado) {
+        this.modelo = modelo;
+        this.estado = estado;
+    }
+
+    @Override
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public boolean getEstado() {
+        return this.estado;
+    }
     
+    @Override
+    public String validarEstado() {
+        if (estado == true) {
+            return "Está encendido";
+        }
+
+        else {
+            return "Está apagado";
+        }
+    }
+    
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }
