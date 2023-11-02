0bd7f872c64ed237ce915541eec0befe1ea5a887
AngelEsquit <esq23221@uvg.edu.gt>
2023-11-01 19:40:33 -0600

    Update Computadora.java

--- Computadora.java
+++ Computadora.java
@@ -1,3 +1,29 @@
-public class Computadora {
+public class Computadora implements DispositivoElectronico{
+    private boolean estado = true;
     
+    @Override
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public boolean getEstado() {
+        return this.estado;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+    @Override
+    public String validarEstado() {
+        if (estado == true) {
+            return "Está encendido";
+        }
+
+        else {
+            return "Está apagado";
+        }
+    }
 }
